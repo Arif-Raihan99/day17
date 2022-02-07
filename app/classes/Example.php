1d83e270f3d540d4215f2a103de5bdcc22cd3a92
Arif Raihan <97831226+Arif-Raihan99@users.noreply.github.com>
2022-02-07 17:34:37 +0600

    Add files via upload

---
+++ app/classes/Example.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\classes;
+use App\classes\Person;
+use App\classes\Operator;
+
+class Example extends Person{
+    public $i;
+    public function index(){
+//        for($this->i =45; $this->i > 22; $this->i--){
+//            echo "<br/>The number is = ".$this->i;
+//        }
+
+
+//        $this->i = 10;
+//        while ($this->i < 20){
+//            echo "The number is = ".$this->i."<br/>";
+//            $this->i++;
+//        }
+
+
+//        $this->i = 10;
+//        do{
+//            echo "The number is = ".$this->i."<br/>";
+//            $this->i++;
+//        }while ($this->i < 20);
+
+          
+
+    }
+}
